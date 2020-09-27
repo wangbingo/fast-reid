61b29170f0c323cca913f0e17cc1384f14bb5f7a
王兵 <wangbing@92-68-1-108.biz.kpn.net>
2020-09-27 13:17:03 +0800

    # _junk_pids = [0, -1]     _junk_pids = [ -1]

--- fastreid/data/datasets/market1501.py
+++ fastreid/data/datasets/market1501.py
@@ -26,7 +26,8 @@ class Market1501(ImageDataset):
         - identities: 1501 (+1 for background).
         - images: 12936 (train) + 3368 (query) + 15913 (gallery).
     """
-    _junk_pids = [0, -1]
+    # _junk_pids = [0, -1]
+    _junk_pids = [ -1]
     dataset_dir = ''
     dataset_url = 'http://188.138.127.15:81/Datasets/Market-1501-v15.09.15.zip'
     dataset_name = "market1501"
