d205be9bdd8792f0b2c1022fa269f20e7e7ec9ae
王兵 <wangbing@92-68-1-108.biz.kpn.net>
2020-10-03 11:47:22 +0800

    shutil.move(file, dst_file_path)     embed()

--- tools/prepare_test-data.py
+++ tools/prepare_test-data.py
@@ -26,34 +26,10 @@ if not os.path.isdir(gallery_save_path):
 files = glob.iglob(src_path + '/query/' "*.png")
 for file in files:
     cls_name = str(random.randint(10,9999))
-    dst_filename = cls_name + '_c' + str(random.randint(1,99)) + 's1_' + file
+    dst_file_path = cls_name + '_c' + str(random.randint(1,99)) + 's1_' + file.split["/"][-1]
+    shutil.move(file, dst_file_path)
     embed()
 
-
-""" 
-
-c = 0
-
-for line in lines:
-    line = line.strip()   # to erase blank
-    line = line.strip('\n')  # to erase \n
-    line_list = line.split(':')
-    
-    src_img_filename  = line_list[0]
-    cls_name          = line_list[1] 
-    
-    src_file_path = src_path + '/images/' + src_img_filename
-
-    dst_filename = cls_name + '_c' + str(random.randint(1,99)) + 's1_' + src_img_filename
-    # dst_filename = cls_name + '_c1s1_' + src_img_filename
-    dst_file_path = train_save_path + '/' + dst_filename
-    
-    shutil.move(src_file_path, dst_file_path)
-    
-    c += 1
-    if c % 10000 == 0:
-        print('{} files copied.'.format(c)) 
-
-print('train dataset completed.  {} files copied.'.format(c)) """
+print('test dataset completed.  {} files copied.'.format(c))
 
 
