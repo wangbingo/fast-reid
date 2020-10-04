427b0ec7d0bfc22bbfc5996f42bf8a53e7e5f92f
王兵 <wangbing@92-68-1-107.biz.kpn.net>
2020-10-04 14:23:54 +0800

    my_run_demo.sh

---
+++ demo/my_run_demo.sh
@@ -0,0 +1,3 @@
+python demo/visualize_result.py --config-file configs/Market1501/bagtricks_R50-ibn.yml \
+--parallel --vis-label --dataset-name 'Market1501' --output logs/Market_vis \
+--opts MODEL.WEIGHTS /content/fast-reid/logs/market1501/bagtricks_R50-ibn.final/model_final.pth
