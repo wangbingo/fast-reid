bed4d95e0fad0c6b1f50ccfc15e951fdfc343a07
王兵 <wangbing@92-68-1-106.biz.kpn.net>
2020-10-09 21:13:41 +0800

    python demo/my_visualize_result.py --config-file configs/Market1501/sbs_S50.yml

--- demo/my_run_demo.sh
+++ demo/my_run_demo.sh
@@ -1,3 +1,3 @@
-python demo/my_visualize_result.py --config-file configs/Market1501/sbs_R50-ibn.yml \
+python demo/my_visualize_result.py --config-file configs/Market1501/sbs_S50.yml \
 --parallel --vis-label --dataset-name 'Market1501' --output logs/Market_vis \
---opts MODEL.WEIGHTS /content/fast-reid/logs/market1501/sbs_R50-ibn/model_final.pth
+--opts MODEL.WEIGHTS /content/fast-reid/logs/market1501/sbs_S50/model_final.pth
