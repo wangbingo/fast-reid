61a14ab5ce509617ab721e0a22a5e81c092ec5ce
王兵 <wangbing@92-68-1-108.biz.kpn.net>
2020-10-16 21:09:44 +0800

    python demo/my_visualize_result_from_distmat.py

--- demo/my_run_demo.sh
+++ demo/my_run_demo.sh
@@ -1,3 +1,3 @@
-python demo/my_visualize_result.py --config-file configs/Market1501/sbs_S101.yml \
+python demo/my_visualize_result_from_distmat.py --config-file configs/Market1501/sbs_S101.yml \
 --parallel --vis-label --dataset-name 'Market1501' --output logs/Market_vis \
 --opts MODEL.WEIGHTS /content/fast-reid/logs/market1501/sbs_S101/model_final.pth
